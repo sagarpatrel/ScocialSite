60a750a4342e2e40b554b704520f1aaa630d28a6
sagar <sagarpatel66666@gmail.com>
2022-11-10 08:20:44 +0530

    sec

--- social_book/settings.py
+++ social_book/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#Activate Django_Heroku.
+django_heroku.settings(locals())  
